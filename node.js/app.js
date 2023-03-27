dc34fbf2fee41da36f7a3ef88ba6fd1eb5f46d25
dom <kimeudom.git@gmail.com>
2023-03-27 16:20:48 +0300

    db_connect successfull

--- node.js/app.js
+++ node.js/app.js
@@ -1,17 +1,21 @@
-const mariadb = require("mariabd");
+const mariadb = require("mariadb");
 
 const pool = mariadb.createPool({
   host: "localhost",
   user: "dom",
-  password: "1ArrowQuill",
+  password: "1ArrowQuill!!",
   database: "smsCB"
 })
 
 
 async function main() {
   try {
-    let conn = await poll.getConnection();
+    let conn = await pool.getConnection();
     let qry = await conn.query("SHOW TABLES");
     console.log(qry);
-  }catch(err)
+  } catch (err) {
+    //console.log("Could not connect");
+  }
 }
+
+main();
