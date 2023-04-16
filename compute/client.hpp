0e7c84512eb809626a87e4ad8a0ee787098a5dba
dom <kimeudom.git@gmail.com>
2023-04-16 18:34:21 +0300

    still work to be done on sending messages

--- compute/client.hpp
+++ compute/client.hpp
@@ -3,12 +3,18 @@ Defines all client method operations that might be needed to classify, move, fin
 */
 #include <iostream>
 #include <math.h>
+#include "./msg.hpp"
+
+
+struct Point{
+  float longitude;
+  float latitude;
+};
 
 class client{
   private:
     // Defining all data memebers required by a class object of member;
-    float longitude;
-    float latitude;
+    Point point;
     std::string tel;
     int cellID;
     int base;
@@ -19,27 +25,27 @@ class client{
     std::string category;
   
   public:
-    client(std::string tel, float longitude, float latitude, int base, int zone, std::string cat);
+    client(std::string tel, Point point, int base, int zone, std::string cat);
     // Returns true if the client is in range of the supplied coordinates
     // Else returns false
-    bool isInRange(float lon, float lat, float radius);
+    bool isInRange(Point centre, float radius);
     // Returns true if the client's state is active or dormant/passive
     // Else if otherwise
     bool isActive();
     // Moves the client to a new base and zone as supplied by the data
     void changeover(int newBase, int newZone);
     // Returns true if the message sent to a user has been repeated
-    bool repeatMsg();
+    bool repeatMsg(Point centre, float radius, msg message);
     // Returns true if the message was sent successfully
     // Else otherwise
     bool sendMsg();
 };
 
-client::client(std::string telephone, float lon, float lat, int bse, int zne, std::string cat){
+client::client(std::string telephone, Point coords, int bse, int zne, std::string cat){
     connectionStatus = 1;
     tel = telephone;
-    longitude = lon;
-    latitude = lat;
+    point.longitude = coords.longitude;
+    point.latitude = coords.latitude;
     base = bse;
     zne = zone;
     category = cat;
@@ -59,6 +65,12 @@ void client::changeover(int newBase, int newZone){
   zone = newZone;
 }
 
-bool client::isInRange( float lon, float lat, float radius){
-  
+bool client::isInRange( Point centre, float radius){
+  // Getting the euchladian distance between the centre of the zone and the client position
+  double euchDistance = sqrt(pow(point.latitude - centre.latitude, 2) + pow(point.longitude - centre.longitude, 2));
+  return (euchDistance <= radius);
+}
+
+void client::sendMsg(){
+
 }
--- compute/msg.hpp
+++ compute/msg.hpp
@@ -22,16 +22,18 @@ class msg{
     std::string message;
     int msgID;
     std::string time;
+    std::string categories;
 
   public:
-    msg(int id, std::string msg);
+    msg(int id, std::string msg, std::string cat);
     std::string getMsg() { return message; };
     std::string getTime() { return time; };
     int getID() { return msgID; };
 };
 
-msg::msg(int id, std::string msg){
+msg::msg(int id, std::string msg, std::string cat){
     message = msg;
     msgID = id;
+    categories = cat;
     time = getTime();
 }
