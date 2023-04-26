eb04a32e83e1ac1777a32a1b6f9395f92dca2e8b
dom <kimeudom.git@gmail.com>
2023-04-26 13:39:17 +0300

    Small changes

--- node/app.js
+++ node/app.js
@@ -5,16 +5,15 @@ const path = require("path");
 const port = 42069;
 const bodyParaser = require("body-parser");
 
-// Setting the listening server
-app.listen(port, (req, res) => {
-  console.log(`Server is live on port ${port} ...`);
-})
-
 // Setting up middleware
 app.use(bodyParaser.json());
 app.use(bodyParaser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, './web/public')));
 
+// Setting the listening server
+app.listen(port, (req, res) => {
+  console.log(`Server is live on port ${port} ...`);
+})
 
 app.get('/client', (req, res) => {
   res.sendFile('/home/dom/imp/project/node/web/index.html');
