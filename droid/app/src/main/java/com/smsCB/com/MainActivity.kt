7472c242a98c760f25cc4b64a6636ce7fca867c0
dom <kimeudom.git@gmail.com>
2023-05-26 10:01:38 +0300

    basic droid app up and running

--- droid/app/src/main/java/com/smsCB/com/MainActivity.kt
+++ droid/app/src/main/java/com/smsCB/com/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.smsCB.com
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
